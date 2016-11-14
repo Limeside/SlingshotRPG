cde449cb7d5548bb279f88c2c89db976b41f9b83
limeside@eyedentitygames.com <limeside@eyedentitygames.com>
2016-11-14 17:16:58 +0900

    Wisp Animation2

--- Assets/Scripts/Wisp.cs
+++ Assets/Scripts/Wisp.cs
@@ -5,21 +5,43 @@ using System;
 public class Wisp : MonoBehaviour {
 
     [SerializeField] public int hp = 0;
+    [SerializeField] public float spd = 1f;
 
     void Awake() {
         _animator = GetComponent<Animator>();
+        _pos = transform.position;
+    }
+
+    void Update() {
+        transform.position = _pos + EightCurve();
+    }
+
+    IEnumerator AngleProcess() {
+        while (!onDead) {
+            if (angle < 360)
+                angle += spd * Time.deltaTime;
+            else
+                angle = 0f;
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Bullet") {
+
+            /*
             Animator p_Animator = other.GetComponent<Animator>();
             Rigidbody2D p_rigidBody = other.GetComponent<Rigidbody2D>();
             other.gameObject.tag = "Untagged";
 
-            p_Animator.Play("Projectile_Hit");
-
             p_rigidBody.gravityScale = 0;
             p_rigidBody.velocity = new Vector3(0, 0, 0);
+            */
+
+            Destroy(other.gameObject);
+
+            StopCoroutine("AngleProcess");
 
             if (hp > 1) {
                 hp--;
@@ -32,15 +54,25 @@ public class Wisp : MonoBehaviour {
         }
     }
 
+    private void Gen() {
+        StartCoroutine("AngleProcess");
+    }
+
     private void Dead() {
-        transform.parent.GetComponent<WispGroup>().wispCount--;
-        transform.parent.GetComponent<WispGroup>().wispGroup.Remove(this.gameObject);
-        transform.parent.GetComponent<WispGroup>().delay = 0;
-
-
         Destroy(gameObject);
     }
 
-    private Animator _animator = null;
+    Vector3 EightCurve() {
 
+        float _x = 1 * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float _y = 1 * Mathf.Sin(angle * Mathf.Deg2Rad) * Mathf.Cos(angle * Mathf.Deg2Rad);
+
+        return new Vector3(_x, _y, 0);
+    }
+
+    private Animator _animator = null;
+    private Vector3 _pos;
+
+    private bool onDead = false;
+    [SerializeField] private float angle = 1f;
 }
